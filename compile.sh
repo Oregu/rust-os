68ae700ced28bdabeee9d29aef0d18797f860071
Oregu <oleg.burykin@gmail.com>
2016-12-19 22:08:29 -0800

    Creating iso and launching it in emulator

--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
-nasm -f elf64 multiboot_header.asm
-nasm -f elf64 boot.asm
-ld -n -o kernel.bin -T linker.ld multiboot_header.o boot.o
+#!/bin/sh
+
+mkdir -p target
+
+nasm -f elf64 -o target/multiboot_header.o multiboot_header.asm
+nasm -f elf64 -o target/boot.o boot.asm
+ld -n -o target/kernel.bin -T linker.ld target/multiboot_header.o target/boot.o
