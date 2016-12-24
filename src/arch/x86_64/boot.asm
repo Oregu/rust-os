42a6eb40d07fdf190b54766449458d9935f95585
Oregu <oleg.burykin@gmail.com>
2016-12-23 16:16:33 -0800

    Load GDT -- Global Descriptor Table

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -12,6 +12,9 @@ start:
     call set_up_page_tables
     call enable_paging
 
+    ; load the 64-bit GDT
+    lgdt [gdt64.pointer]
+
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
@@ -135,6 +138,15 @@ error:
     mov dword [0xb8008], 0x4f204f20
     mov byte  [0xb800a], al
     hlt
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+    dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
+    dq (1<<44) | (1<<47) | (1<<41) ; data segment
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
 
 section .bss
 align 4096
